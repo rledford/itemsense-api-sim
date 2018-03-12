7c1fe88fd62c1995967729543ad62b253e615b80
rledford <ryan.c.ledford@gmail.com>
2018-03-12 08:17:47 -0500

    added settings - routes now called simulator fn to handle req

--- routes.js
+++ routes.js
@@ -1,22 +1,10 @@
-var sim = require('./simulator');
+var sim = require('./simulator').simulator;
 
-module.exports = (app) => {
-	app.get('/', (req, res) => {
-		res.send('<h1>ItemSense API Simulator</h1>');
-	});
-	app.get('/itemsense/data/v1/items/show', (req, res) => {
-		res.json({
-			nextPageMarker: null,
-			items: [
-				{
-					jobId: 'xxxxxxx-xxx-xxx-xxx-xxxxxxxxxxxx',
-					lastModifiedTime: (new Date()).toISOString(),
-					tagId: '',
-					zone: 'Some Zone',
-					epc: '1234567',
-					presenceConfidence: 'HIGH'
-				}
-			]
-		});
-	});
+module.exports = app => {
+  app.get('/', (req, res) => {
+    res.send('<h1>ItemSense API Simulator</h1>');
+  });
+  app.get('/itemsense/data/v1/items/show', (req, res) => {
+    res.json(sim.showItems(req.query || {}));
+  });
 };
--- simulator.js
+++ simulator.js
@@ -1,13 +1,45 @@
-class Simulator {
-	constructor() {
-	}
-	
-	start() {
-		console.log('simulator started');
-	}
+const fs = require('fs');
+const data = JSON.parse(
+  fs.readFileSync('./settings.json', { encoding: 'utf-8' })
+);
 
-	showItems(query) {
-	}
+class Simulator {
+  constructor() {
+    (this.epcs = {}), (this.zones = {});
+  }
+
+  start() {
+    (this.epcs = data.epcs || {}), (this.zones = data.zones || {});
+  }
+
+  getRandomZone() {
+    const index = Math.floor(Math.random() * this.zones.length);
+    return this.zones[index] || 'ABSENT';
+  }
+
+  showItems(query) {
+    let epcs = this.epcs.slice();
+    if (query.epcPrefix) {
+      epcs = epcs.filter(epc => {
+        return epc.startsWith(query.epcPrefix);
+      });
+    }
+
+    const data = {};
+    data.nextPageMarker = null;
+    data.items = epcs.map(epc => {
+      return {
+        jobId: 'xxxxxxx-xxx-xxx-xxx-xxxxxxxxxxxx',
+        lastModifiedTime: new Date().toISOString(),
+        tagId: '',
+        zone: this.getRandomZone(),
+        epc: epc,
+        presenceConfidence: 'HIGH'
+      };
+    });
+
+    return data;
+  }
 }
 
 var simulator = new Simulator();

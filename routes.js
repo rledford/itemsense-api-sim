70dd9eac75cad777e85f50e62991ca5258a2c71e
rledford <ryan.c.ledford@gmail.com>
2018-03-15 22:27:19 -0500

    added queue PUT handler

--- routes.js
+++ routes.js
@@ -7,4 +7,10 @@ module.exports = app => {
   app.get('/itemsense/data/v1/items/show', (req, res) => {
     res.json(sim.showItems(req.query || {}));
   });
+  app.put('/itemsense/data/v1/queues', (req, res) => {
+    res.json({
+      serverUrl: 'amqp://localhost:5672/%2F',
+      queue: '1127b6d0c96f6c55d42e54b390f9a6c50fc4911b956c1a3128a2e26c3f6481cd'
+    });
+  });
 };

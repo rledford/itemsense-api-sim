0486c80c410678dc6e0e78d8f34eea6cc804604b
Ubuntu <ubuntu@ip-172-31-41-253.us-east-2.compute.internal>
2018-03-21 03:54:22 +0000

    added jobs info

--- routes.js
+++ routes.js
@@ -7,10 +7,39 @@ module.exports = app => {
   app.get('/itemsense/data/v1/items/show', (req, res) => {
     res.json(sim.showItems(req.query || {}));
   });
-  app.put('/itemsense/data/v1/queues', (req, res) => {
+
+  app.get('/itemsense/authentication/v1/token', (req, res) => {
+    res.json({token: '123456-123456-123456-123456'});
+  });
+  
+  app.put('/itemsense/data/v1/items/queues', (req, res) => {
     res.json({
       serverUrl: 'amqp://localhost:5672/%2F',
-      queue: '1127b6d0c96f6c55d42e54b390f9a6c50fc4911b956c1a3128a2e26c3f6481cd'
+      queue: '1127b6d0c96f6c55d42e54b390f9a6c50fc4911b956c1a3128a2e26c3f6481cd-' + Date.now()
+    });
+  });
+
+  app.get('/itemsense/control/v1/jobs/show', (req, res) => {
+    const now = new Date().toISOString();
+    res.json({ array:
+        [{
+            id: '12e1kj12e',
+            status: 'RUNNING',
+            readerName: ['Zone1', 'Zone2'],
+            failedReaderName: [],
+            creationTime:  now,
+            lastActivity: now,
+            activityDuration: Math.floor(Math.random() * 1000),
+            errorOccured: false,
+            errors: [{time: now, message: 'Some error message'}],
+            maxErrors: 10,
+            stopReason: 'USER_REQUESTED_GRACEFUL',
+            facilities: ['Lake Charles'],
+            job: {},
+            instanceMetadata: {},
+            lastHeartbeatTime: now,
+            startAttempts: 1
+        }]
     });
   });
 };
